f1a6e5c8b800d74f3481fde813192504763f7ff0
Emgan <emgan29@gmail.com>
2021-05-26 11:51:17 +0200

    strated the css for the php page, i'll finish it later

--- tableau_de_bord.php
+++ tableau_de_bord.php
@@ -13,53 +13,55 @@
 		<button><a href="./temperature.html">Temperature</a></button>
 	</div>
 	<div class = "lumiere">
-		<button> <a href="#">Lumières</a></button>
+		<button> <a href="./lumiere.html">Lumières</a></button>
 	</div>
 	<div class = "tdb">
-		<button> <a href="./tableau_de_bord.php">Tableau de Bord</a></button>
+		<button> <a href="#">Tableau de Bord</a></button>
 	</div>
 	<div>
 		<fieldset>
-			<legend>Tableau de bord Affichage</legend>
-			<center>
-				<p>Temperature</p>
-			</center>
-			<table border="1px">
-				<tbody>
-					<tr>
-						<td>Capteur 1 (temp 1)</td>
-						<td>Capteur 2 (temp 2)</td>
-						<td>Capteur 3 (temp 3)</td>
-						<td rowspan="2"> <img src="./images/capteur_temp.png" alt="capteur temperature"> </td>
-					</tr>
-					<tr>
-						<td>10.25</td>
-						<td>11.32</td>
-						<td>20.45</td>
-					</tr>
-				</tbody>
-			</table>
-
-			<center>
+			<legend>
+				Tableau de bord
+				<span>Affichage</span>
+			</legend>
+			<div>
+				<p class= temperatureP>Temperature</p>
+				<table class="temperatureTable">
+					<tbody>
+						<tr>
+							<td>Capteur 1 (temp 1)</td>
+							<td>Capteur 2 (temp 2)</td>
+							<td>Capteur 3 (temp 3)</td>
+							<td rowspan="2" class="capteurTemp"> <img src="./images/capteur_temp.png" alt="capteur temperature"> </td>
+						</tr>
+						<tr>
+							<td>10.25</td>
+							<td>11.32</td>
+							<td>20.45</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div class="lumiereTable">
 				<p>Lumière</p>
-			</center>
-			<table border="1px">
-				<tbody>
-					<tr>
-						<td>Capteur 1 (lum 1)</td>
-						<td>20%</td>
-						<td rowspan="3"> <img src="./images/capteur_lum.png" alt="capteur lumière"> </td>
-					</tr>
-					<tr>
-						<td>capteur 2 (lum 2)</td>
-						<td>40%</td>
-					</tr>
-					<tr>
-						<td>capteur 3 (lum 3)</td>
-						<td>90%</td>
-					</tr>
-				</tbody>
-			</table>
+				<table>
+					<tbody>
+						<tr>
+							<td>Capteur 1 (lum 1)</td>
+							<td>20%</td>
+							<td rowspan="3"> <img src="./images/capteur_lum.png" alt="capteur lumière"> </td>
+						</tr>
+						<tr>
+							<td>capteur 2 (lum 2)</td>
+							<td>40%</td>
+						</tr>
+						<tr>
+							<td>capteur 3 (lum 3)</td>
+							<td>90%</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 			<a href="https://www.isen.fr/" class="logo"><img src="./images/logo_isen.jpg" alt="logo isen"></a>
 		</fieldset>
 	</div>
