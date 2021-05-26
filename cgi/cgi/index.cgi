0aa6a1cfe4ade59571cb14eeae1594cd06a96bfe
Emgan <emgan29@gmail.com>
2021-05-26 09:32:58 +0200

    resolved the directory mess that i've created

--- cgi/cgi/index.cgi
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Content-Type: text/html; charset=utf-8
-
-Scripts CGI OK !
-BRAVO ;)"
--- cgi/cgi/test-example-post.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-REQUEST_METHOD='POST'
-CONTENT_LENGTH=70
-QUERY_STRING='choix_capteur=lum1&range_lum=50&on_off=on&password=1234&code_maison=A'
-export CONTENT_LENGTH
-export REQUEST_METHOD
-echo $QUERY_STRING | ./config.cgi
